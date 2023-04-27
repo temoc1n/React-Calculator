3826027a9ff91aa7aee3e6337e3104ab8ce3dd1d
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-04-27 14:57:27 +0200

    Calculator component

---
+++ src/main/Calculator.jsx
@@ -0,0 +1,12 @@
+import { Component } from "react";
+import './Calculator.css';
+
+export default class Calculator extends Component {
+    render(){
+        return(
+            <div className="calculator">
+                <h1>asd</h1>
+            </div>
+        )
+    }
+}

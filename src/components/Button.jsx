7456d72aac87b11a38a3f6f2e32b8641a6d0b738
System Administrator <root@Dannyyusis-MacBook-Pro.local>
2023-04-27 17:10:45 +0200

    Buttons and clicks

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,21 @@
 import './Button.css'
 
 export default props => {
+
+    let classes = 'button '
+    
+    classes += props.operation ? 'operation' : '';
+    classes += props.double ? 'double' : '';
+    classes += props.triple ? 'triple' : '';
+
     return(
-        <button className="button">{props.label}</button>
+        <button 
+        className={classes} 
+        onClick={() => props.click && props.click(props.label)}
+        >
+
+            {props.label}
+        
+        </button>
     )
 }
